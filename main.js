969ef799758e99e2b6b9c4060a0f2053e50ed3bc
heavens0116 <88336889+heavens0116@users.noreply.github.com>
2021-08-22 03:10:22 +0900

    PORTFOLIO-HEAVEN

---
+++ main.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+// Make navbar transparent when it is on the top
+document.addEventListener('scroll',()=>{
+  // console.log(window.scrollY);
+  // 스크롤 포지션을 알 수 있다
+  // console.log(`navbarHeight:${navbarHeight}`);
+  if (window.scrollY > navbarHeight){
+    navbar.classList.add('navbar-dark');
+  } else {
+    navbar.classList.remove('navbar-dark');
+  }
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+
+
+
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home_container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down 
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll',() => {
+  if (window.scrollY > homeHeight /2) {
+    arrowUp.classList.add('visible');
+
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+// Projects 
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category_btn.selected');
+  active.classList.remove('selected');
+  const target =  
+  e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if(filter == '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  },300);
+
+  //다 같은 값이 나오는 함수들. 위에 forEach가 조금 더 간편
+  // console.log(`--------------`);
+  // for(let project of projects) {
+  //   console.log(project);
+  // }
+
+  // console.log(`--------------`);
+  // let project;
+  // for(let i = 0; i < projects.length; i++){
+  //   project = projects[i];
+  //   console.log(project);
+  // }
+
+  console.log(filter) 
+});
+
+
+
+
+
+
+
+//유틸리티 함수는 제일 밑
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior:'smooth'});
+}
+
